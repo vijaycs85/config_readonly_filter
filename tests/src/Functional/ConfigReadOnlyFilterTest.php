3f3188e25e15a09e24676b268ddccf271c470272
Vijaya Chandran Mani <vijaycs85@gmail.com>
2017-11-30 18:03:37 +0000

    Adding test module with default filter

--- tests/src/Functional/ConfigReadOnlyFilterTest.php
+++ tests/src/Functional/ConfigReadOnlyFilterTest.php
@@ -37,4 +37,29 @@ class ConfigReadOnlyFilterTest extends ConfigReadOnlyFilterTestBase {
     $session->pageTextNotContains('This form will not be saved because the configuration active store is read-only.');
   }
 
+  /**
+   * Tests module with filter.
+   */
+  public function testConfigReadOnlyFilterModule() {
+    // Enable read-only mode.
+    $this->turnOnReadOnlySetting();
+    $session = $this->assertSession();
+    // Make sure menu is not editable.
+    $this->drupalGet('/admin/structure/menu/manage/admin');
+    $session->pageTextContains('This form will not be saved because the configuration active store is read-only.');
+
+    // Disable read-only mode to enable test module.
+    $this->turnOffReadOnlySetting();
+    // Install test module with filter.
+    \Drupal::service('module_installer')->install(['config_readonly_filter_test']);
+    $this->rebuildContainer();
+
+    // Enable read-only mode.
+    $this->turnOnReadOnlySetting();
+    // Make sure menu is editable.
+    $this->drupalGet('/admin/structure/menu/manage/admin');
+    $session->pageTextNotContains('This form will not be saved because the configuration active store is read-only.');
+
+  }
+
 }
